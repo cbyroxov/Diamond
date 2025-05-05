af89325754092627b5a3e08c806399490855e5a4
Zach <31392083+Cocoatwix@users.noreply.github.com>
2025-05-05 08:54:59 -0300

    Choice button added for selecting the category of thing to modify in the BIOS.

--- diamond.py
+++ diamond.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 """
 PlayStation BIOS ROM Editor, programmed with love.
+
+Resources used:
+"Overview of wxPython" by The wxPython Team: https://wxpython.org/pages/overview/
+"Sizers Overview" by The wxPython Team: https://docs.wxpython.org/sizers_overview.html
+
+"wx.Choice" by The wxPython Team: https://docs.wxpython.org/wx.Choice.html#wx-choice
+
+"Layout management in wxPython" by Jan Bodnar: https://zetcode.com/wxpython/layout/
 """
 
 import wx
@@ -17,6 +25,23 @@ class Diamond(wx.Frame):
 		"""
 		super(Diamond, self).__init__(*args, **kw)
 		
+		'''
+		This defines all the different things the
+		user can control about the BIOS.
+		
+		Each dictionary in the dictionary defines a
+		"category" of options from which the user can
+		click through. This prevents every single options
+		from being on-screen at once.
+		'''
+		BIOSproperties = {
+			"Category 1": {},
+			"Category 2": {},
+			"Category 3": {},
+			"Category 4": {},
+			"Category 5": {}
+		}
+		
 		#Wrapper panel for everything
 		wrapperPanel = wx.Panel(self)
 		wrapperSizer = wx.BoxSizer(wx.VERTICAL)
@@ -28,6 +53,15 @@ class Diamond(wx.Frame):
 		bannerBitmap = wx.Bitmap("banner.bmp", type=wx.BITMAP_TYPE_BMP)
 		bannerImage = wx.StaticBitmap(bannerPanel, wx.ID_ANY, bannerBitmap)
 		wrapperSizer.Add(bannerPanel, flag=wx.ALIGN_CENTRE_HORIZONTAL) #Flag aligns banner horizontally
+		
+		#Present options for user to choose from 
+		# (i.e. which properties of the BIOS to change)
+		categoryPanel = wx.Panel(wrapperPanel)
+		categoryChoice = wx.Choice(categoryPanel, 
+								   choices=list(BIOSproperties.keys()),
+								   size=wx.Size(500, 20))
+
+		wrapperSizer.Add(categoryPanel, border=5, flag=wx.TOP|wx.ALIGN_CENTRE_HORIZONTAL)
 		
 		#Fit window to size of sizer
 		wrapperSizer.SetSizeHints(self)
